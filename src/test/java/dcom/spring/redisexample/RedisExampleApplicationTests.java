8a820a3f46b87a953016365a32d96cb87b41c7c3
justkode <sobu0715@gmail.com>
2022-06-28 05:20:42 +0900

    feat: Project init

---
+++ src/test/java/dcom/spring/redisexample/RedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dcom.spring.redisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
